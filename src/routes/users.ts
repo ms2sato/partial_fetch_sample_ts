6547fe6cc19480013c6d3e458085c38aa4235513
ms2sato <ms2.sato@gmail.com>
2021-05-11 20:41:11 +0900

    refactor samples

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -8,59 +8,56 @@ const messages: string[] = [
 ]
 
 /* GET users listing. */
-router.get(
-  "/",
-  function (
-    _req: express.Request,
-    res: express.Response,
-    _next: express.NextFunction,
-  ) {
-    res.render('user', { titleCounter: titleCounter++, messages })
-  },
-);
+router.get("/", (
+  _req: express.Request,
+  res: express.Response,
+  _next: express.NextFunction,
+) => {
+  res.render('user', { titleCounter: titleCounter++, messages })
+})
 
-router.get('/time', function (
+router.get('/time', (
   req: express.Request,
   _res: express.Response,
   _next: express.NextFunction
-) {
+) => {
   req.partials()
     .add('#output', '/user/time.pug')
     .render()
 })
 
-router.get('/title', function (
+router.get('/title', (
   req: express.Request,
   _res: express.Response,
   _next: express.NextFunction
-) {
+) => {
   req.partials()
     .add('#title', '/user/title.pug', { titleCounter: titleCounter++ })
     .render()
 })
 
-router.get('/multi', function (
+router.get('/multi', (
   req: express.Request,
   _res: express.Response,
   _next: express.NextFunction
-) {
+) => {
   req.partials()
     .add('#output', '/user/time.pug')
     .add('#title', '/user/title.pug', { titleCounter: titleCounter++ })
     .render()
 })
 
-router.post('/messages', function (
+router.post('/messages', (
   req: express.Request,
   _res: express.Response,
   _next: express.NextFunction
-) {
+) => {
   const message = (req.body as Record<string, string>).message
   const partials = req.partials()
-  if(!message || message.length === 0) {
+  if (!message || message.length === 0) {
     return partials
-      .add('form', '/user/form.pug', { 
-        message, err: { errors: [{ message: 'メッセージが空です' }] } 
+      .add('form', '/user/form.pug', {
+        message, err: { errors: [{ message: 'メッセージが空です' }] }
       })
       .render()
   }
